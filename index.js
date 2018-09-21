51c692d8fd2da933a247900286cdb588cd913c06
Alois Barreras <aloisbarreras@icloud.com>
2018-09-21 18:37:37 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const Kong = require('@tropos/kong-admin-api-client');
+
+class ServerlessPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+    this.kong = null;
+
+    this.hooks = {
+      'after:deploy:deploy': this.afterDeployFunctions.bind(this),
+    };
+  }
+
+  /**
+   * @description hook to after deployment
+   *
+   * @return {Promise}
+   */
+
+  async afterDeployFunctions() {
+    this.stage = this.serverless.service.provider.stage || 'dev';
+    this.region = this.serverless.service.provider.region || 'us-east-1';
+    this.providerName = this.serverless.service.provider.name;
+
+    const config = (this.serverless.config.serverless.service.custom || {}).kong || {}
+
+    this.kong = new Kong({ adminAPIURL: config.admin_api_url || 'http://localhost:8001' });
+
+    // Create the dummy service for Lambda
+    const service = await this.kong.services.createOrUpdate('lambda-dummy-service', {
+      url: 'http://localhost:8001',
+    });
+
+    // create the routes in Kong for each function with a Kong event
+    for (const f of Object.keys(this.serverless.service.functions)) {
+      const func = this.serverless.service.functions[f];
+
+      // find an event named 'kong'
+      for (const event of func.events) {
+        if (event.kong) {
+          // create the route
+          const route = await this.kong.routes.create({
+            service: { id: service.id },
+            paths: event.kong.paths,
+            methods: event.kong.methods,
+            hosts: event.kong.hosts,
+            protocols: event.kong.protocols,
+          });
+
+          // add the lambda plugin to the route
+          const plugin = await this.kong.routes.addPlugin({
+            routeId: route.id,
+            name: 'aws-lambda',
+            config: {
+              aws_key: event.kong.aws_key || config.aws_key,
+              aws_secret: event.kong.aws_secret || config.aws_secret,
+              aws_region: event.kong.region || config.region || this.region,
+              function_name: func.name,
+            },
+            enabled: true,
+          });
+        }
+      }
+    }
+  }
+}
+
+module.exports = ServerlessPlugin;
